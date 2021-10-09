fcdc3fb785c081f233fc3779a4c1dc9dfa843d9b
vladislavrepkin <vladislav_repkin@epam.com>
2021-10-09 16:40:05 +0200

    init

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const GRAY_COLOR = '#808080';
 export const RED_COLOR = '#f44336';
-export const BLUE_COLOR = '#f44336';
+export const BLUE_COLOR = '#2196f3';
 export const CARDS_COUNT = 8;
 export const TOTAL_CARDS = 25;
